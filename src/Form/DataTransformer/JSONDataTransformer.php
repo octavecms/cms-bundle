23aa8f1c2a5726e58e0b8ab6ccfa7e4c54c1e778
Igor Lukashov <igor.lukashov@videinfra.com>
2018-02-09 17:22:44 +0300

    JSONDataTransformer

---
+++ src/Form/DataTransformer/JSONDataTransformer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace VideInfra\CMSBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+
+/**
+ * @author Igor Lukashov <igor.lukashov@videinfra.com>
+ */
+class JSONDataTransformer implements DataTransformerInterface
+{
+    /**
+     * @param mixed $value
+     * @return mixed
+     */
+    public function transform($value)
+    {
+        if (!is_string($value)) {
+            $output = $value;
+        }
+        else {
+            $output = json_decode($value, true);
+        }
+
+        return $output;
+    }
+
+    /**
+     * @param mixed $value
+     * @return mixed|string
+     */
+    public function reverseTransform($value)
+    {
+        if (is_string($value)) {
+            return $value;
+        }
+
+        return json_encode($value);
+    }
+}
