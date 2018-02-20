7966b6b6cab561daa9253b119aa5bc8486b254f5
Igor Lukashov <igor.lukashov@videinfra.com>
2018-02-20 16:42:42 +0300

    Collection type

--- src/Form/Type/CollectionType.php
+++ src/Form/Type/CollectionType.php
@@ -4,12 +4,35 @@ namespace VideInfra\CMSBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType as BaseCollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use VideInfra\CMSBundle\Form\DataTransformer\JSONDataTransformer;
 
 /**
  * @author Igor Lukashov <igor.lukashov@videinfra.com>
  */
 class CollectionType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer(new JSONDataTransformer());
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
+
+            $data = $event->getData();
+
+            if (is_string($data)) {
+                $data = json_decode($data, true);
+            }
+
+            $event->setData($data);
+        }, 40);
+    }
+
     /**
      * @return string
      */
