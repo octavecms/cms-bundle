928396d847ea33c8d5adce94aa2429e7c7646386
Kaspars Zuks <kaspars.zuks@videinfra.com>
2018-04-06 12:16:32 +0300

    Removed debug output

--- src/Resources/public/sitemap/src/scripts/components/error-message.js
+++ src/Resources/public/sitemap/src/scripts/components/error-message.js
@@ -27,7 +27,6 @@ export default class ErrorMessage {
     }
 
     handleVisibilityChange (visible) {
-        console.log('visible:', visible);
         if (visible) {
             this.$container.modal('show');
         } else {
@@ -36,7 +35,6 @@ export default class ErrorMessage {
     }
 
     handleMessageChange (message) {
-        console.log('message:', message);
         this.$text.text(message);
     }
 }
