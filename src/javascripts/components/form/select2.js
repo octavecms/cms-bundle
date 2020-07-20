1acd908c53c53dc0f02336cf883af01711180557
Kaspars Zuks <kaspars.zuks@videinfra.com>
2020-07-20 15:58:55 +0300

    Select2 with image

--- src/javascripts/components/form/select2.js
+++ src/javascripts/components/form/select2.js
@@ -1,1 +1,41 @@
 import select2 from 'select2';
+
+function formatSelection (state) {
+    const image = $(state.element).data('image');
+
+    if (!state.id || !image) {
+        return state.text;
+    } else {
+        const $state = $('<span class="select2-selection__image"><img src="" alt="" /> <span></span></span>');
+
+        $state.find('img').attr('src', image).attr('alt', state.text);
+        $state.find('span').text(state.text);
+
+        return $state;
+    }
+};
+
+function formatResult (state) {
+    const image = $(state.element).data('image');
+
+    if (!state.id || !image) {
+        return state.text;
+    } else {
+        const $state = $('<span class="select2-results__option__image"><img src="" alt="" /> <span></span></span>');
+
+        $state.find('img').attr('src', image).attr('alt', state.text);
+        $state.find('span').text(state.text);
+
+        return $state;
+    }
+};
+
+
+const originalSelect2Plugin = $.fn.select2;
+
+$.fn.select2 = function (options = {}) {
+    originalSelect2Plugin.call($(this), $.extend({
+        templateResult: formatResult,
+        templateSelection: formatSelection,
+    }, options));
+};
