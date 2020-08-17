7a281a9e7be87202d638f87e888b250ca7e384af
Kaspars Zuks <kaspars.zuks@videinfra.com>
2020-08-17 11:24:23 +0300

    Editor toolbar fix

--- src/javascripts/components/form/tinymce/quickblock-collapsed.js
+++ src/javascripts/components/form/tinymce/quickblock-collapsed.js
@@ -4,14 +4,22 @@ function isImage (node) {
 function isEditable (editor, node) {
     return editor.dom.getContentEditableParent(node) !== 'false';
 };
+function isEmtpy (node) {
 
+}
+
+/**
+ * When text selection is collapsed show toolbar with "Bold", "Italic", etc. buttons too
+ * 
+ * @param {object} editor Editor instance
+ */
 export default function setupQuickblockCollapsedToolbar (editor) {
     const collapsedToolbarItems = editor.getParam('quickbars_collapsed_toolbar', '');
 
     if (collapsedToolbarItems && collapsedToolbarItems.trim().length > 0) {
         editor.ui.registry.addContextToolbar('textscollapsed', {
             predicate: function (node) {
-                return !isImage(node) && editor.selection.isCollapsed() && isEditable(editor, node);
+                return !isImage(node) && editor.selection.isCollapsed() && isEditable(editor, node) && !editor.dom.isEmpty(node);
             },
             items: collapsedToolbarItems,
             position: 'node',
