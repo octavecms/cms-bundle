017cb76050c67699f9f887bc71cb0081e5351516
Igor Lukashov <igor.lukashov@videinfra.com>
2020-12-15 15:48:28 +0300

    Sitemap generator fixes

--- src/Service/SitemapSource.php
+++ src/Service/SitemapSource.php
@@ -19,17 +19,22 @@ class SitemapSource implements SourceInterface
     /* @var string|null */
     protected $host;
 
+    /** @var array */
+    protected $locales;
+
     /**
-     * 
+     *
      * @param RouterInterface $router
      * @param PageRepository $pageRepository
      * @param string|null $host
+     * @param $locales
      */
-    public function __construct(RouterInterface $router, $pageRepository, $host)
+    public function __construct(RouterInterface $router, $pageRepository, $host, $locales)
     {
         $this->router = $router;
         $this->pageRepository = $pageRepository;
         $this->host = rtrim($host, '/');
+        $this->locales = $locales;
     }
 
     /**
@@ -43,10 +48,12 @@ class SitemapSource implements SourceInterface
         $pages = $this->pageRepository->findIncludeInSitemap();
                 
          foreach($pages as $page) {
-            $items[] = new Item(
-                $this->generateUrl($page->getName()),
-                $page->getUpdatedAt()
-            );
+             foreach ($this->locales as $locale) {
+                 $items[] = new Item(
+                     $this->generateUrl($page->getName(), ['_locale' => $locale]),
+                     $page->getUpdatedAt()
+                 );
+             }
         }
 
         return $items;
@@ -54,12 +61,13 @@ class SitemapSource implements SourceInterface
 
     /**
      * @param string $name
+     * @param array $params
      * @return string
      */
-    private function generateUrl($name)
+    private function generateUrl($name, $params = [])
     {
         return $this->host
-            ? $this->host . $this->router->generate($name)
-            : $this->router->generate($name, [], UrlGeneratorInterface::ABSOLUTE_URL);
+            ? $this->host . $this->router->generate($name, $params)
+            : $this->router->generate($name, $params, UrlGeneratorInterface::ABSOLUTE_URL);
     }
 }
