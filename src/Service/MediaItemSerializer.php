0fb0017e27769d4e0b124dfc3e54617380d59d51
Daler Boboev <daler.boboev@videinfra.com>
2023-12-15 17:45:05 +0300

    #66481 image resize for thumbnails

--- src/Service/MediaItemSerializer.php
+++ src/Service/MediaItemSerializer.php
@@ -10,15 +10,20 @@ use Symfony\Component\Mime\MimeTypes;
  */
 class MediaItemSerializer
 {
+    /** @var ImageProcessor $imageProcessor */
+    protected $imageProcessor;
+
     /** @var string */
     protected $uploadDir;
 
     /**
      * MediaItemSerializer constructor.
+     * @param ImageProcessor $imageProcessor
      * @param $uploadDir
      */
-    public function __construct($uploadDir)
+    public function __construct(ImageProcessor $imageProcessor, $uploadDir)
     {
+        $this->imageProcessor = $imageProcessor;
         $this->uploadDir = $uploadDir;
     }
 
@@ -58,7 +63,7 @@ class MediaItemSerializer
             'id' => $item->getId(),
             'isImage' => $isImage,
             'icon' => 'fa-file',
-            'image' => $item->getPath(),
+            'image' => $this->imageProcessor->resize($item->getPath(), 300, 300),
             'path' => $item->getPath(),
             'filename' => $item->getName(),
             'parent' => ($item->getCategory()) ? $item->getCategory()->getId() : 'root',
