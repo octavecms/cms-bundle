5db5e6a6a45b91a5f6b8be6e445f2c0839c144ec
Kaspars Zuks <kaspars.zuks@videinfra.com>
2024-04-09 13:36:52 +0300

    Fixed errors when collection widget is inside other collection widget

--- src/Resources/public/blocks/src/scripts/components/collection-widget.js
+++ src/Resources/public/blocks/src/scripts/components/collection-widget.js
@@ -1,7 +1,5 @@
 import $ from 'lib/jquery';
-
 import map from 'lodash/map';
-import each from 'lodash/each';
 
 
 const NAMESPACE = 'collection';
@@ -10,6 +8,10 @@ const ORDER_INPUT_CSS_SELECTOR = 'input[type="hidden"][name*="[position]"]';
 
 const REGEX_MATCH_NUMBERS = /\d+/g;
 
+const SELECTOR_LIST = '.js-collection-list';
+const SELECTOR_LIST_TITLE = '.js-collection-list-item-title';
+const SELECTOR_ADD = '.js-collection-add';
+const SELECTOR_REMOVE = '.js-collection-remove';
 
 class CollectionWidget {
 
@@ -28,8 +30,10 @@ class CollectionWidget {
 
     _init () {
         const $element = this.$element;
-        const $list    = this.$list    = $element.find('.js-collection-list');
-        const $button  = this.$button  = $element.find('.js-collection-add');
+        const $list    = this.$list    = $element.find(SELECTOR_LIST).not($element.find(SELECTOR_LIST + ' ' + SELECTOR_LIST));
+
+        // Find button, but no sub-list button
+        const $button  = this.$button  = $element.find(SELECTOR_ADD).not(this.$list.find(SELECTOR_ADD));
 
         // Item counter
         this.index = this.options.orderCssSelector ? this._getMaxIndex() : $list.children().length;
@@ -48,7 +52,7 @@ class CollectionWidget {
         $button.on(`click.${ NAMESPACE }`, this._handleAddItem.bind(this));
 
         // "Remove" button click
-        $list.on(`click.${ NAMESPACE }`, '.js-collection-remove', this._handleRemoveItem.bind(this));
+        $list.on(`click.${ NAMESPACE }`, SELECTOR_REMOVE, this._handleRemoveItem.bind(this));
 
         // When this whole widget is removed from DOM trigger 'destroy'
         $element.on(`remove.${ NAMESPACE }`, this.destroy.bind(this));
@@ -67,7 +71,6 @@ class CollectionWidget {
 
         this.$element = this.$list = this.$button = this.options = null;
     }
-
 
     /**
      * Order
@@ -138,7 +141,7 @@ class CollectionWidget {
      * Add item
      */
 
-    _handleAddItem () {
+    _handleAddItem (event) {
         const $html = $(this._generateItemHTML());
         this.$list.append($html);
         this.$list.find('script[type="text/javascript"]').remove();
@@ -151,7 +154,13 @@ class CollectionWidget {
         var index = this.index++;
         var html = this.$list.data('prototype');
 
-        html = html.replace(/__name__/g, index);
+        // Since it's possible that inside collection items will other collections
+        // for each place where we have "__name__" we need to replace only first occurance
+        // &#x5B; === [
+        // &#x5D; === ]
+        html = html.replace(/([a-z0-9-_\[\]]|&#x5B;|&#x5D;)*__name__([a-z0-9-_\[\]]|&#x5B;|&#x5D;)*/g, function (match) {
+            return match.replace('__name__', index);
+        });
 
         return html;
     }
@@ -161,9 +170,12 @@ class CollectionWidget {
      */
 
     _handleRemoveItem (e) {
-        const $item = $(e.target).closest('.js-collection-list > li');
-        $item.remove();
-        this._updateList();
+        if (this._validateItem(e.target)) {
+            const $item = $(e.target).closest(SELECTOR_LIST + ' > li');
+
+            $item.remove();
+            this._updateList();
+        }
     }
 
     /**
@@ -171,12 +183,19 @@ class CollectionWidget {
      */
 
     _handleCollapseItem (e) {
-        const $item = $(e.target).closest('.js-collection-list > li');
-        this._updateListItemTitle($item);
+        if (this._validateItem(e.target)) {
+            const $item = $(e.target).closest(SELECTOR_LIST + ' > li');
+            this._updateListItemTitle($item);
+        }
     }
     _updateListItemTitle ($item) {
-        const $title = $item.find('.js-collection-list-item-title');
-        $title.text(this._getListItemTitle($item));
+        if (this._validateItem($item)) {
+            // Make sure we pick only title for this collection item, not all titles inside
+            // the sub-collection (if such exists)
+            const $subtitles = $item.find(SELECTOR_LIST + ' ' + SELECTOR_LIST_TITLE);
+            const $title = $item.find(SELECTOR_LIST_TITLE).not($subtitles);
+            $title.text(this._getListItemTitle($item));
+        }
     }
 
     _getListItemTitle ($item) {
@@ -214,6 +233,21 @@ class CollectionWidget {
         }
 
         return '';
+    }
+
+    /**
+     * Validate event target
+     * Returns true if target is somewhere inside collection item
+     * Returns false if target is outside collection item or if it's inside sub-collection item
+     * 
+     * @param {jQuery|HTMLElement} target Target
+     * @returns {boolean}
+     */
+    _validateItem (target) {
+        const $list = $(target).closest(SELECTOR_LIST);
+
+        // Make sure it's not sub collection widget
+        return this.$list.is($list);
     }
 }
 
